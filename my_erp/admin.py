f0ca4fdbf6d0a764a055a016b8c5620c3b15f4e8
Pavlick3000 <p.zagornyak@olimplift.com>
2024-09-11 12:01:44 +0300

    Добавлена первая модель

--- my_erp/admin.py
+++ my_erp/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import NomencBook
 
+# class MyExistingTableAdmin(admin.ModelAdmin):
+#     list_display = ('idr_ref', 'other_field')  # Поля, которые будут отображаться в списке
+#     search_fields = ('idr_ref', 'other_field')  # Поля, по которым будет производиться поиск
+#     readonly_fields = ('idr_ref',)  # Поля, которые будут доступны только для чтения
 # Register your models here.
+
+class NomencBookAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.write()
+
+admin.site.register(NomencBook, NomencBookAdmin)
